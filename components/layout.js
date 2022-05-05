b0f1a99eadbc6ffca9060bbb2304f84da04df7aa
Thooooor <46197373+Thooooor@users.noreply.github.com>
2022-05-05 10:28:08 +0800

    add layout

---
+++ components/layout.js
@@ -0,0 +1,50 @@
+import Footer from './Footer'
+import Header from './Header'
+import Profile from "./profile"
+import PageHead from './Pagehead'
+import Main from './main'
+import News from './news'
+import siteConfig from "../site.config"
+import styles from './styles.module.css'
+
+export default function Layout({ biograph }) {
+    const mainConfig = {
+        author: siteConfig.author,
+        description: siteConfig.description
+    }
+
+    const profileConfig = {
+        author: siteConfig.author,
+        author_cn: siteConfig.author_cn,
+        descriptions: siteConfig.descriptions,
+        socials: siteConfig.socials,
+    }
+    
+    const footerConfig = {
+        author: siteConfig.author,
+    }
+    
+    const headerConfig = {
+        author: siteConfig.author,
+        contents: siteConfig.contents,
+    }
+    
+    const pageHeadConfig = {
+        author: siteConfig.author,
+        title: siteConfig.title,
+    }
+
+    return (
+        <div className={styles.container}>
+            <PageHead pageHeadConfig={pageHeadConfig}  />
+            <Header headerConfig={headerConfig} />
+            <Profile profileConfig={profileConfig} />
+            <News />
+            <Main 
+                biography={biograph} 
+                mainConfig={mainConfig} 
+            />
+            <Footer footerConfig={footerConfig} />
+        </div>
+    )
+}
