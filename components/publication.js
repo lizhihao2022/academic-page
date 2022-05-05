61e7d46de5dc0c66bfb33dc2477e78a0bdfef260
Thooooor <46197373+Thooooor@users.noreply.github.com>
2022-05-05 10:22:42 +0800

    add publication

---
+++ components/publication.js
@@ -0,0 +1,27 @@
+import publications from "../data/publications"
+import styles from './styles.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilePdf, faFileCode,faCode } from "@fortawesome/free-solid-svg-icons"
+import { faBookmark } from "@fortawesome/free-regular-svg-icons"
+
+export default function Publication({ pubConfig }) {
+    return (
+        <div className={styles.chapter} id="publications">
+            <h1 className={styles.chapter_title}>Publications</h1>
+            <div className={styles.pub_list}>
+                {publications.map(({ title,authors,description,year,pdf,code }) => (
+                    <div className="" key="">
+                        <div className={styles.pub_title}>
+                            <FontAwesomeIcon icon={faBookmark} />
+                            &nbsp;
+                            <a href={pdf}>{title}</a>
+                            <a href={code}><FontAwesomeIcon icon={faFileCode} /></a>
+                        </div>
+                        <div className={styles.pub_des} dangerouslySetInnerHTML={{ __html: description }} />
+                        <div className={styles.pub_author} dangerouslySetInnerHTML={{ __html: authors }} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
---
+++ data/publications.js
@@ -0,0 +1,13 @@
+const publications = [
+    {
+        title: "Hyperbolic Graph Neural Networks: A Review of Methods and Applications",
+        authors: "Menglin Yang, Min Zhou, <b>Zhihao Li</b>, Jiahong Liu, Lujia Pan, Hui Xiong, Irwin King",
+        description: "arXiv preprint arXiv:2202.13852",
+        year: "2022",
+        pdf: "https://arxiv.org/pdf/2202.13852",
+        code: "https://github.com/marlin-codes/HGNNs",
+        tag: []
+    },
+]
+
+module.exports = publications
