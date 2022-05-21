900e72663e7847bccd17b39499e3e401ae586c56
Thooooor <46197373+Thooooor@users.noreply.github.com>
2022-05-22 02:02:28 +0800

    support md in news

--- data/news.js
+++ data/news.js
@@ -1,11 +1,7 @@
 const news = [
     {
-        date: "2022-05-19",
-        content: "Paper 'HICF: Hyperbolic Informative Collaborative Filtering' has been accepted at KDD 2022 Research Track."
-    },
-    {
-        date: "2022-05-05",
-        content: "Brand new self-designed personal homepage online."
+        date: "05/2022",
+        content: "One paper has been accepted at **KDD 2022** Research Track."
     },
 ]
 
